99b60498cb9c062a82874c01abda47c06cb68cec
geisa-paim <geisafigueiredo45@gmail.com>
2023-10-06 13:07:33 -0300

    update

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function Abrirmenu() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imgs/menu_off.svg";
+        document.querySelector('.icon').src = "imgs/menu.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "imgs/close_off.svg";
+        document.querySelector('.icon').src = "imgs/close.svg";
     }
 }
